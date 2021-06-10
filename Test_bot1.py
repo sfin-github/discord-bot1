07c9f0faa042a75f00e0f6f254430000dbbb457f
sfin-github <85697111+sfin-github@users.noreply.github.com>
2021-06-10 10:10:51 -0600

    Add files via upload

---
+++ Test_bot1.py
@@ -0,0 +1,67 @@
+import os
+import discord
+from dotenv import load_dotenv, find_dotenv
+
+##BASEDIR = os.path.abspath(os.path.dirname(__file__))
+##env_path = os.path.join(BASEDIR, '.env')
+##load_dotenv(dotenv_path = env_path)
+load_dotenv()
+
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+client = discord.Client()
+
+
+
+@client.event
+async def on_ready():
+    print(client.user, 'has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+##    print(client.user, 'is connected to the following guild: '
+##        f'{guild.name}(id: {guild.id})')
+##    for member in guild.members:
+##        print(member)
+
+
+
+
+
+def print_shrek():
+    return('''SHREK DETECTED
+            ⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀
+            ⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀
+            ⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆
+            ⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆
+            ⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆
+            ⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿
+            ⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉
+            ⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+            ⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇
+            ⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇
+            ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿
+            ⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇
+            ⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃
+            ⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉''')
+
+
+
+
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:       #Ignore messages from this bot
+        if('s' in message.content and 'h' in message.content and 'r' in message.content and 'e' in message.content and 'k' in message.content):
+            await message.channel.send(print_shrek())
+
+
+
+
+
+client.run(TOKEN)
+
+
+
